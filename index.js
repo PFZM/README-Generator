12e93b7b1cc9880b828b0367c202bee5a9546dcb
PFZM <pfzm@hotmail.com>
2021-10-16 18:20:11 +1100

    added questions array

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-// TODO: Include packages needed for this application
-// const inquirer = require("inquirer");
-// const fs = require("fs");
+//Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const badge = require("./utils/generateBadge")
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+// Array of questions for user input
+const questions = [
+    "Project name:",
+    "Description of the project",
+    "Installations instructions:",
+    "Usage information:",
+    "License:",
+    "Contribution guidelines:",
+    "Test intructions:",
+    "Githihib user:",
+    "Email:"
+];
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-// // TODO: Create a function to initialize app
-// function init() => {}
+// TODO: Create a function to initialize app
+function init() => {}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
